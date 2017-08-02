b1d519a935ae5e41a09e1d09df32573589656908
ct-rogers <mtmventures@gmail.com>
2017-08-02 09:27:10 -0400

    first commit completed

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const mustacheExpress = require("mustache-express")
+
+app.engine("mst", mustacheExpress())
+app.set("views", "./views")
+app.set("view engine", "mst")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const authenticate = (request, response, next) => {
+  if (request.body.userName === "foo" && request.body.password === "berries") {
+    next()
+  } else {
+    response.render("home")
+  }
+}
+
+app.get("/", (request, response) => {
+  response.render("home")
+})
+
+console.log(authenticate)
+app.use(authenticate)
+
+app.post("/login", (request, response) => {
+  response.render("login", request.body)
+})
+
+app.listen(3000, () => {
+  console.log("Listening on Port 3000")
+})
